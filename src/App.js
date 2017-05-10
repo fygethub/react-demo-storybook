0a62bb22b9204bd6386faee778fd5c237cfab4bf
fygethub <13658399878@163.com>
2017-05-10 21:33:17 +0800

    增加redux逻辑

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
+import { Provider } from 'react-redux';
 import Routes from './router/Routers'
 import darkBaseTheme from "material-ui/styles/baseThemes/lightBaseTheme";
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import './styles/reset.css';
+import { receiveAutoComplete, getBookList} from './redux/action';
+import Loading from './components/commont/Loading';
+import store from './redux/store';
+
+store.subscribe(() =>
+    console.log(store.getState())
+)
+
+store.dispatch(receiveAutoComplete('he'));
+
+setTimeout(function () {
+    store.dispatch(receiveAutoComplete('大'));
+},1000)
+
+setTimeout(function () {
+    store.dispatch(getBookList('hello world'));
+},2000)
+
 
 /*引用tap事件适配移动端*/
 injectTapEventPlugin();
@@ -18,9 +37,12 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Routes />
-      </div>
+        <Provider store={store}>
+          <div className="App">
+                <Loading />
+                <Routes />
+          </div>
+        </Provider>
     );
   }
 }
