c816876013444147f577a8f90d51805cabbae948
fygethub <13658399878@163.com>
2017-05-14 19:28:43 +0800

    添加推荐列表和历史记录

---
+++ src/components/BookListItem.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Card } from 'material-ui/Card';
+import '../styles/search.css';
+
+/*搜索结果列表*/
+class BookListItem extends Component {
+    constructor(props){
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+    /*这里换成箭头函数不知道为什么this指向不对*/
+    handleClick(e) {
+        e.stopPropagation();
+        this.props.item.onFetchBookIntro( this.props.item.bookId);
+    }
+
+    render() {
+        const { item } = this.props;
+        return (
+            <li className="bookListItem">
+                <Link to="bookIntro"
+                      onClick={this.handleClick}
+                >
+                    <Card
+                        containerStyle={{width:'100%'}}
+                        style={{width:'100%'}}
+                    >
+                        <div className="card-content">
+                            <div>
+                                <img src={item.imgUrl} alt="img" className="itemImg" />
+                            </div>
+                            <div className="bookIntro">
+                                <h3>{item.hTitle}</h3>
+                                <p className="subtitle ">
+                                    <span>{item.latelyFllower}人在追 |</span> <span>{item.retentionRatio}%读者留存 |</span> <span> {item.author}</span>
+                                </p>
+                            </div>
+                        </div>
+                        <p className="shortIntro">{item.shortIntro}</p>
+                    </Card>
+                </Link>
+            </li>
+        )
+    }
+}
+
+export default BookListItem;
---
+++ src/components/RecommendAndHistory.js
@@ -0,0 +1,65 @@
+import React,{ Component } from 'react';
+import Chip from 'material-ui/Chip';
+import '../styles/search.css';
+
+
+/*推荐列表*/
+/* eslint-disable */
+class RecommendAndHistory extends Component {
+    render(){
+        let style = {backgroundColor:"white",border:'1px solid var(--gray-lighter)'};
+        let redStyle = {backgroundColor:"white",border:'1px solid var(--brand-danger)',color:'var(--brand-danger)'}
+        return (
+            <div className="recommend">
+                <div className="h"><h3>大家都在搜</h3></div>
+                <ul className="cards">
+                    {/*<li>
+                     <Chip
+                     onTouchTap={()=>{}}
+                     style={{backgroundColor:"white",border:'1px solid var(--gray-lighter)'}}
+                     >狼与兄弟</Chip>
+                     </li>*/}
+                    {
+                        this.props.otherSearch.map((name,i) =>(
+                            <li key={i}>
+                                <Chip
+                                    onTouchTap={()=>{this.props.onSearchBook(name)}}
+                                    labelStyle={i<=1 ? {color:'red'}: null}
+                                    style={i<=1? redStyle: style}
+                                >{name}</Chip>
+                            </li>
+                        ))
+                    }
+                </ul>
+                <div className="searchHistory">
+                    <div className="searchHistoryHead">
+                        <a className="leftHead">搜索历史</a>
+                        <a className="rightHead">清除历史</a>
+                    </div>
+                    <ul className="cards">
+                        {
+                            this.props.historyList.map((name,i)=> (
+                                <li key={i}>
+                                    <Chip
+                                        onTouchTap={()=>{this.props.onSearchBook(name)}}
+                                        onRequestDelete={() => this.props.onRequestDelete(name)}
+                                    >{ name }</Chip>
+                                </li>
+                            ))
+                        }
+                        {/*<li>
+                         <Chip
+                         onRequestDelete={()=>{}}
+                         >狼与兄弟</Chip>
+                         </li>
+                         */}
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+
+export default RecommendAndHistory;
